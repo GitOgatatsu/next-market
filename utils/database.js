9cdf5df152f9d4a00bd93e7fe33edd6086950b32
herasake <ogawa.ogatatsu.tatsuya.jp@gmail.com>
2023-04-11 10:31:50 +0900

    2023/04/11 10:31

---
+++ utils/database.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+
+	try {
+		await mongoose.connect("mongodb+srv://ogatatsu:puku123puku@cluster0.mq7iilc.mongodb.net/appDataBase3?retryWrites=true&w=majority");
+		console.log("Success: Connected to MongoDB");
+	} catch {
+		console.log("Failure: Unconnected to MongoDB");
+		throw new Error();
+	}
+
+};
+
+export default connectDB;
