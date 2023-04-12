a32c9b26a360ae2973942a6c2bef472b29a569af
herasake <ogawa.ogatatsu.tatsuya.jp@gmail.com>
2023-04-12 19:16:18 +0900

    2023/04/12 19:16

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
-const ReadAllItems = () => {
+import Link from "next/link";
+import Image from "next/image";
+
+
+
+const ReadAllItems = (props) => {
 	return (
 		<div>
-			<h1 className="h1-style">こんにちは</h1>
-			<h3>さようなら</h3>
+			<div>
+				{props.allItems.map(item =>
+					<Link href="" key={item._id}>
+							<Image src={item.image} width="750" height="500" alt="item-image" />
+							<div>
+								<h2>{item.price}</h2>
+								<h3>{item.title}</h3>
+								<p>{item.description}.substring(0, 80)</p>
+							</div>
+					</Link>
+				)}
+			</div>
 		</div>
 	);
 };
 
 export default ReadAllItems;
+
+export const getServerSideProps = async () => {
+	const response = await fetch("http://localhost:3000/api/item/readall");
+	const allItems = await response.json();
+	return {
+		props: allItems
+	};
+};
